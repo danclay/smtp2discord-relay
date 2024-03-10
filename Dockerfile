a529f6da2698cd8df52d212778fffc05e2ddbe46
danclay <18021681+danclay@users.noreply.github.com>
2024-03-10 12:10:16 -0700

    Moved to Typescript

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,35 @@
 # syntax=docker/dockerfile:1
 
-# Comments are provided throughout this file to help you get started.
-# If you need more help, visit the Dockerfile reference guide at
-# https://docs.docker.com/go/dockerfile-reference/
-
 ARG NODE_VERSION=18.18.2
+
+FROM node:${NODE_VERSION}-alpine as builder
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+COPY package*.json ./
+
+RUN npm ci
+
+COPY . .
+
+RUN npm run build
 
 FROM node:${NODE_VERSION}-alpine
 
-# Use production node environment by default.
 ENV NODE_ENV production
-
-
-WORKDIR /usr/src/app
-
-# Download dependencies as a separate step to take advantage of Docker's caching.
-# Leverage a cache mount to /root/.npm to speed up subsequent builds.
-# Leverage a bind mounts to package.json and package-lock.json to avoid having to copy them into
-# into this layer.
-RUN --mount=type=bind,source=package.json,target=package.json \
-    --mount=type=bind,source=package-lock.json,target=package-lock.json \
-    --mount=type=cache,target=/root/.npm \
-    npm ci --omit=dev
-
-# Run the application as a non-root user.
 USER node
 
-# Copy the rest of the source files into the image.
-COPY . .
+# Create app directory
+WORKDIR /usr/src/app
 
-# Expose the port that the application listens on.
+# Install app dependencies
+COPY package*.json ./
+
+RUN npm ci --omit=dev
+
+COPY --from=builder /usr/src/app/dist ./dist
+
 EXPOSE 2525
-
-# Run the application.
-CMD node index.js
+CMD [ "node", "dist/index.js" ]
---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import { SMTPServer } from 'smtp-server';
+import { simpleParser } from 'mailparser';
+import axios from 'axios';
+
+const DEFAULT_WEBHOOK_URL = process.env.WEBHOOK_URL || process.env.DEFAULT_WEBHOOK_URL;
+const SMTP_PORT = process.env.SMTP_PORT || 2525;
+const SMTP_USERNAME = process.env.SMTP_USERNAME;
+const SMTP_PASSWORD = process.env.SMTP_PASSWORD;
+
+if (!DEFAULT_WEBHOOK_URL) {
+    console.error('WEBHOOK_URL environment variable is required');
+    process.exit(1);
+}
+
+const getWebhookMapping = () => {
+    const MAPPED_WEBHOOKS = process.env.MAPPED_WEBHOOKS;
+    if (!MAPPED_WEBHOOKS) return {};
+    const eachMapping = MAPPED_WEBHOOKS.split(',').map(item => item.split('|').map(subitem => subitem.trim()));
+    return Object.fromEntries(eachMapping);
+}
+const webhookMappings = getWebhookMapping();
+
+if (!SMTP_USERNAME || !SMTP_PASSWORD) {
+    console.error('SMTP_USERNAME and SMTP_PASSWORD environment variables are required');
+    process.exit(1);
+}
+
+const smtpServer = new SMTPServer({
+    disabledCommands: ['STARTTLS'],
+    secure: false,
+    authOptional: false, // Require authentication
+    onAuth(auth, session, callback) {
+        if (auth.username === SMTP_USERNAME && auth.password === SMTP_PASSWORD) {
+            return callback(null, { user: auth.username }); // Authentication succeeded
+        } else {
+            return callback(new Error('Invalid username or password')); // Authentication failed
+        }
+    },
+    onData(stream, session, callback) {
+        simpleParser(stream)
+            .then(parsed => {
+                const subject = parsed.subject || 'No subject';
+                const from = parsed.from ? (parsed.from.text || 'Unknown sender') : 'Unknown sender';
+                const getRecipient = () => {
+                    if (parsed.to) {
+                        if (Array.isArray(parsed.to)) {
+                            if (parsed.to[0].value[0].address) {
+                                return parsed.to[0].value[0].address;
+                            }
+                        } else {
+                            if (parsed.to.value[0].address) {
+                                return parsed.to.value[0].address;
+                            }
+                        }
+                    }
+                    return "unknown-recipient@unknown.local";
+                }
+                const recipient = getRecipient();
+                
+                const embed = {
+                    author: {
+                        name: from
+                    },
+                    title: subject,
+                    description: parsed.text,
+                    timestamp: parsed.date || new Date(),
+                    footer: {
+                        text: `Recipient: ${recipient}`
+                    }
+                }
+
+                const webhookUrl = webhookMappings[recipient] || DEFAULT_WEBHOOK_URL;
+                // Forwarding message to Discord
+                axios.post(webhookUrl, {
+                    embeds: [embed]
+                })
+                .then(() => {
+                    console.log(`Email from ${from} to ${recipient}: Forwarded to Discord`);
+                })
+                .catch(err => {
+                    console.error(`Email from ${from} to ${recipient}: Failed to send message to Discord`, err);
+                });
+                callback(); // Accept the message
+            })
+            .catch(err => {
+                console.error('Error parsing email', err);
+                callback(err);
+            });
+    }
+});
+
+smtpServer.listen(SMTP_PORT, () => {
+    console.log(`SMTP server listening on port ${SMTP_PORT}`);
+});
